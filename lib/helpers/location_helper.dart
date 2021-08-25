fa08c981dc0d36d5e443c2493ee4ca95e1bfc24a
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2021-08-26 00:47:41 +0300

    Static Map
    I'm using Yandex Maps API to render the map

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,7 @@
+// const GOOGLE_API_KEY = 'AIzaSyBg9yn5JtQgKRFbg6FCTy4ewbF24kRuAYI';
+
+class LocationHelper {
+  static String generateImageLocation({double? latitude, double? longitude}) {
+    return 'https://static-maps.yandex.ru/1.x/?lang=en_US&size=500,350&l=map&ll=$longitude,$latitude&z=15&pt=$longitude,$latitude,round';
+  }
+}
