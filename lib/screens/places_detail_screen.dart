efbebd1b4a6d1f74528f927ae6d3eca4f4ea74aa
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-11 22:59:07 +0300

    Update places_detail_screen.dart

--- lib/screens/places_detail_screen.dart
+++ lib/screens/places_detail_screen.dart
@@ -9,6 +9,7 @@ class PlaceDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     final filterId = ModalRoute.of(context)!.settings.arguments;
     final selectedPlace = Provider.of<Places>(context, listen: false)
         .findById(filterId.toString());
@@ -16,33 +17,52 @@ class PlaceDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(selectedPlace.title!),
         centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
       body: Column(
         children: [
-          Container(
-            height: 250,
-            width: double.infinity,
-            child: Image.file(
-              selectedPlace.image!,
-              fit: BoxFit.cover,
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(80),
+              bottomRight: Radius.circular(80),
+            ),
+            child: Container(
+              height: deviceSize.height * .65,
               width: double.infinity,
+              margin: EdgeInsets.only(bottom: 20),
+              child: Image.file(
+                selectedPlace.image!,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
             ),
           ),
-          SizedBox(height: 10),
           Text(selectedPlace.location!.address!, textAlign: TextAlign.center),
           SizedBox(height: 10),
-          TextButton(
-            child: Text('View on Map'),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  fullscreenDialog: true,
-                  builder: (ctx) => MapsScreen(
-                    initialLocation: selectedPlace.location!,
-                  ),
-                ),
-              );
-            },
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                child: Text('Other places like this'),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+              ),
+              OutlinedButton(
+                child: Text('View on Map'),
+                style: OutlinedButton.styleFrom(shape: StadiumBorder()),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      fullscreenDialog: true,
+                      builder: (ctx) => MapsScreen(
+                        initialLocation: selectedPlace.location!,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         ],
       ),
